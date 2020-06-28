7fb3685272b58db195513a88f6b7d932a17a9c42
Dennis Gyurics <dgyurics@gmail.com>
2020-06-27 17:58:03 -0700

    refactor

--- script.py
+++ script.py
@@ -3,26 +3,32 @@ import sys
 bits_list = []
 
 def valid_bits(bits):
+    ERROR_MESSAGE = 'error: input must consist of four binary digits'
+    if len(input) != 4:
+        print ERROR_MESSAGE
+        return False
     for x in bits:
-        x = int(x)
-        if x > 1 or x < 0:
-           print 'error: input must consist of 1s and 0s only'
-           return False
+        if x != '1' and x != '0':
+            print ERROR_MESSAGE
+            return False
     return True
 
 def analyze():
-    return
+    # create probability array/list
+    print bits_list
 
-# commands
-# enter four bits
-# enter analyze
-# enter exit
+banner =  '-------------------------------------------------\n'
+banner += '-- Type analyze to predict the next four bits  --\n'
+banner += '-- Type exit to exit                           --\n'
+banner += '-------------------------------------------------\n'
+print banner
+
 while True:
-    print 'enter a command'
+    print 'Enter four bits, or a command:'
     input = raw_input()
     if input == 'analyze':
         analyze()
-    elif input == 'exit' or len(input) != 4:
+    elif input == 'exit':
         quit();
     elif valid_bits(input):
         bits_list.append(input)
